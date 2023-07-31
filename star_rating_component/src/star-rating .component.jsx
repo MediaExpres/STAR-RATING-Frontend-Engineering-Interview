8d456c6ad49d03cb207f844e4170216bcdef71a5
Mihai Pavelescu <media.expres@gmail.com>
2023-07-31 23:16:07 +0300

    building

---
+++ star_rating_component/src/star-rating .component.jsx
@@ -0,0 +1,12 @@
+const starRating = () => {
+    const maxRating =5;
+
+    return (
+        {
+            [...Array(5)].map((_, idx) => <p key={idx}>{idx + 1</p>)
+        }
+    )
+
+}
+
+export default starRating;
